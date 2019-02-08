ceecc92c4075a52b3e73ceb778fbd9af4a911a9f
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2019-02-08 11:26:58 -0500

    timeofday

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,22 @@
 import React from "react";
 
-const App = () => <h1>hello world!</h1>;
+const App = () => {
+  const timeOfDay = 13; //new Date().getHours();
+
+  let greeting;
+  if (timeOfDay < 12) {
+    greeting = "good morning";
+  } else if (timeOfDay > 12 && timeOfDay < 17) {
+    greeting = "good afternoon";
+  } else {
+    greeting = "good evening";
+  }
+
+  return (
+    <div>
+      <h1>{greeting}</h1>
+    </div>
+  );
+};
 
 export default App;
